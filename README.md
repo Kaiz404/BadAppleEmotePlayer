df40d958f8e21f03a07a01b80702dbb648c2a019
Kaiz404 <84890519+Kaiz404@users.noreply.github.com>
2022-02-19 11:27:07 +0800

    Added finishing comments and edits

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Bad Apple Emote Player
 ## Setup
 
-Before you start, please make sure you have [ffmpeg](https://ffmpeg.org/download.html) install and added into your device PATHS
+Before you start, please make sure you have [ffmpeg](https://ffmpeg.org/download.html) installed and added into your device PATHS
 First, clone this repository with the command:
 
 ```git clone https://github.com/Kaiz404/bad-apple.git```
@@ -10,9 +10,9 @@ install python modules:
 
 ```pip install -r requirements.txt```
 
-Open command prompt within the empty "frames" directory then run the following command to extract all frames from the music video:
+Open command prompt within the "frames" directory then run the following command to extract all frames from the music video:
 
 ```ffmpeg -i ../badappleMV.mkv -vf fps=30 %04d.png```
 
 ## The fun part
-In the emotes folder add any emote or emoji you want to convert into a bad apple mv, then change the emote name in badapple.py line 17-18 according to your desired emotes. You can also change the emote and frame dimensions if you wish to, just make sure its ratio is constant. Now run badapple.py and let your device run.
+In the emotes folder add any emote or emoji you want to convert into a bad apple mv, then change the emote name in badapple.py(line 25-26) according to your desired emotes. You can also change the emote and frame dimensions if you wish to(line 12-13), just make sure its ratio is constant. Now run badapple.py and let your device do the rest, the final video can be found in the "results" directory
--- badapple.py
+++ badapple.py
@@ -4,24 +4,35 @@ from PIL import Image
 import glob
 import os
 import ffmpeg
+import sys
 
 
 
-emote_w, emote_h = 16, 16
-frame_w, frame_h = 24, 18
+# change output resolution here
+emote_w, emote_h = 16, 16  # the pixel count of the emotes
+frame_w, frame_h = 48, 36  # the resolution of the emotes (e.g 48, 36 means 48 emotes in the x-axis and 36 emotes in the y-axis), the higher the numbers the better the resolution, but at the price of unrecognizable emotes
 
 emote_size = (emote_w, emote_h)
 frame_res = (frame_w, frame_h)
 
+proceed = input(f"Final video resolution will be {emote_w*frame_w} x {emote_h*frame_h}, proceed? (y/n): ")
 
-white = Image.open("amogus.png").resize(size=emote_size)
-black = Image.open("black.png").resize(size=emote_size)
+if proceed.lower() != "y":
+    sys.exit()
 
 
+# change your emotes here, emote_light will be displayed for every white pixel and emote_dark will be displayed every black pixel (black.png by default)
+emote_light = "emote.png"
+emote_dark = "black.png"
 
+
+white = Image.open(f"./emotes/{emote_light}").resize(size=emote_size)
+black = Image.open(f"./emotes/{emote_dark}").resize(size=emote_size)
+
+os.chdir("frames")
+
+# process every frame in the "frames" directory then outputs new frames into "outputframes"
 for frame in tqdm(glob.glob("*.png"), desc="Processing frames...", unit="frames"):
-
-    os.chdir("../frames")
 
     processed_frame = Image.new('RGB', (frame_w*emote_w, frame_h*emote_h))
     
@@ -42,16 +53,21 @@ for frame in tqdm(glob.glob("*.png"), desc="Processing frames...", unit="frames"
 
             processed_frame.paste(im=result, box=(0,y*emote_h))
 
-        os.chdir("../outputframes")
-
-        processed_frame.save(fp=f"{frame}.png")
+        processed_frame.save(fp=f"../outputframes/{frame}")
 
 
-ffmpeg.input('%04d.png', framerate=30).output('output.mp4').overwrite_output().run()
+os.chdir("../outputframes")
+# compiles all new frames into a single video
+(
+    ffmpeg
+    .input('%04d.png', framerate=30)
+    .output('output.avi')
+    .overwrite_output()
+    .run()
+)
 
-os.chdir("../result")
-
-input_video = ffmpeg.input("../outputframes/output.mp4")
+os.chdir("../results")
+# merge output video and audio to create the final result
+input_video = ffmpeg.input("../outputframes/output.avi")
 input_audio = ffmpeg.input("../audio.aac")
-
 ffmpeg.concat(input_video, input_audio, v=1, a=1).output('result.mp4').run()
